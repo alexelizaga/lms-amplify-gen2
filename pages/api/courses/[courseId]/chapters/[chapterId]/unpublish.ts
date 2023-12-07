ebf42934b4c4b1cf6b3774e028e9e5f52ac8618b
alexelizaga <aelizag@corp.vodafone.es>
2023-12-07 11:12:53 +0100

    feat: publish and unpublish

---
+++ pages/api/courses/[courseId]/chapters/[chapterId]/unpublish.ts
@@ -0,0 +1,92 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getCurrentUser } from "aws-amplify/auth/server";
+
+import { reqResBasedClient, runWithAmplifyServerContext } from "@/utils";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PATCH") {
+    return unpublishChapter(req, res);
+  }
+
+  return res.status(400).json({
+    message: "Bad request",
+  });
+}
+
+const unpublishChapter = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { userId } = await runWithAmplifyServerContext({
+      nextServerContext: { request: req, response: res },
+      operation: (contextSpec) => getCurrentUser(contextSpec),
+    });
+
+    if (!userId) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const { chapterId: id = "" } = req.query;
+    const { courseId: courseId = "" } = req.query;
+
+    if (typeof id !== "string" || typeof courseId !== "string") {
+      return res.status(400).json({
+        message: "Bad request",
+      });
+    }
+
+    const unpublishedChapter = await runWithAmplifyServerContext({
+      nextServerContext: { request: req, response: res },
+      operation: async (contextSpec) => {
+        const { data: updatedChapter } =
+          await reqResBasedClient.models.Chapter.update(contextSpec, {
+            id,
+            isPublished: false,
+          });
+        return updatedChapter;
+      },
+    });
+
+    const publishedChaptersInCourse = await runWithAmplifyServerContext({
+      nextServerContext: { request: req, response: res },
+      operation: async (contextSpec) => {
+        const { data: courses } = await reqResBasedClient.models.Chapter.list(
+          contextSpec,
+          {
+            filter: {
+              isPublished: {
+                eq: "true",
+              },
+            },
+          }
+        );
+        return courses;
+      },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await runWithAmplifyServerContext({
+        nextServerContext: { request: req, response: res },
+        operation: async (contextSpec) => {
+          const { data: updateCourse } =
+            await reqResBasedClient.models.Course.update(contextSpec, {
+              userId,
+              courseId,
+              isPublished: false,
+            });
+          return updateCourse;
+        },
+      });
+    }
+
+    return res.status(200).json(unpublishedChapter);
+  } catch (error) {
+    console.log("[COURSE_ID]", error);
+    return res.status(500).json({
+      message: "Internal Error",
+    });
+  }
+};
