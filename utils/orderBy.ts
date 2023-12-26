1654368da7ec16dfa41c2d1dafa6c1437a60bcfb
alexelizaga <aelizag@corp.vodafone.es>
2023-12-26 19:45:46 +0100

    feat: categories order by name

--- utils/orderBy.ts
+++ utils/orderBy.ts
@@ -10,6 +10,16 @@ export function orderByTitle(a: { title: string }, b: { title: string }) {
   return 0;
 }
 
+export function orderByName(a: { name: string }, b: { name: string }) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+}
+
 export function orderByPosition(
   a: { position: number },
   b: { position: number }
