608c7f67ff919131d59514c10209f19d797572b2
alexelizaga <aelizag@corp.vodafone.es>
2023-12-29 10:57:49 +0100

    feat: remove swr

--- providers/index.ts
+++ providers/index.ts
@@ -1,5 +1,4 @@
 export { ConfettiProvider } from "./confetti-provider";
 export { MyThemeProvider } from "./my-theme-provider";
 export { ReactQueryProvider } from "./react-query-provider";
-export { SWRProvider } from "./swr-provider";
 export { ToastProvider } from "./toaster-provider";
--- providers/swr-provider.tsx
+++
@@ -1,9 +0,0 @@
-import { SWRConfig } from "swr";
-
-type Props = {
-  children: JSX.Element | JSX.Element[];
-};
-
-export const SWRProvider: React.FC<Props> = ({ children }) => {
-  return <SWRConfig>{children}</SWRConfig>;
-};
