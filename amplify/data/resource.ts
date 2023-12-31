fe04b895ee0fab638cf284c06d874e058609c9ce
alexelizaga <alexelizaga@icloud.com>
2023-12-31 08:31:12 +0100

    feat: fix data base and apis

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,55 +1,47 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-      done: a.boolean(),
-      priority: a.string(),
-    })
-    .authorization([a.allow.owner()]),
   Course: a
     .model({
+      category: a.belongsTo("Category"),
+      chapters: a.hasMany("Chapter"),
       courseId: a.id().required(),
-      userId: a.id().required(),
-      title: a.string().required(),
       description: a.string(),
       image: a.string(),
       imageUrl: a.string(),
-      price: a.float(),
       isPublished: a.boolean().default(false),
-      category: a.belongsTo("Category"),
-      chapters: a.hasMany("Chapter"),
+      price: a.float(),
+      title: a.string().required(),
+      userId: a.id().required(),
     })
-    .identifier(["courseId", "userId"])
     .authorization([a.allow.owner(), a.allow.private().to(["read"])]),
   Category: a
     .model({
+      courses: a.hasMany("Course"),
       icon: a.string(),
       name: a.string().required(),
-      courses: a.hasMany("Course"),
     })
     .authorization([a.allow.owner(), a.allow.private().to(["read"])]),
   Chapter: a
     .model({
-      title: a.string().required(),
-      description: a.string(),
-      video: a.string(),
-      streamUrl: a.string(),
-      streamStartTime: a.string(),
-      streamEndTime: a.string(),
-      position: a.integer().required(),
-      isPublished: a.boolean().default(false),
-      isFree: a.boolean().default(false),
       course: a.belongsTo("Course"),
+      description: a.string(),
+      isFree: a.boolean().default(false),
+      isPublished: a.boolean().default(false),
+      position: a.integer().required(),
+      streamEndTime: a.string(),
+      streamStartTime: a.string(),
+      streamUrl: a.string(),
+      title: a.string().required(),
+      video: a.string(),
     })
     .authorization([a.allow.owner(), a.allow.private().to(["read"])]),
   UserProgress: a
     .model({
       chapterId: a.id().required(),
-      userId: a.id().required(),
       courseId: a.id().required(),
       isCompleted: a.boolean().default(false),
+      userId: a.id().required(),
     })
     .authorization([a.allow.owner(), a.allow.private().to(["read"])]),
 });
